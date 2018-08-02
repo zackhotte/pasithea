151828b1739e6527716aaa678ec235c11542a6bb
zackhot <zack@zackhot.com>
2018-08-02 06:47:59 -0400

    Added routes to access shopping cart data

--- src/main/java/com/github/zackhotte/pasithea/controller/ProductRestController.java
+++ src/main/java/com/github/zackhotte/pasithea/controller/ProductRestController.java
@@ -3,11 +3,15 @@ package com.github.zackhotte.pasithea.controller;
 import com.github.zackhotte.pasithea.model.AuthorRepository;
 import com.github.zackhotte.pasithea.model.Book;
 import com.github.zackhotte.pasithea.model.BookRepository;
+import com.github.zackhotte.pasithea.model.ShoppingCart;
+import com.github.zackhotte.pasithea.model.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -15,16 +19,29 @@ public class ProductRestController {
 
     private BookRepository bookRepository;
     private AuthorRepository authorRepository;
+    private ShoppingCartRepository shoppingCartRepository;
 
     @Autowired
-    public ProductRestController(BookRepository bookRepository, AuthorRepository authorRepository) {
+    public ProductRestController(BookRepository bookRepository, AuthorRepository authorRepository,
+                                 ShoppingCartRepository shoppingCartRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
+        this.shoppingCartRepository = shoppingCartRepository;
     }
 
     @GetMapping(path = "/{productId}")
     public Book productInformation(@PathVariable Long productId) {
         return bookRepository.findOne(productId);
+    }
+
+    @GetMapping(path = "/shoppingcart")
+    public List<ShoppingCart> shoppingCart() {
+        return shoppingCartRepository.findAll();
+    }
+
+    @GetMapping(path = "/shoppingcart/{shoppingCartId}")
+    public ShoppingCart getItemInShoppingCart(@PathVariable Long shoppingCartId) {
+        return shoppingCartRepository.findOne(shoppingCartId);
     }
 
 }
