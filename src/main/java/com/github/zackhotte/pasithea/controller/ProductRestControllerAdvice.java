dbaf3486b980b5539924b41223878c2f0b8a6844
zackhot <zack@zackhot.com>
2018-08-02 13:24:48 -0400

    Added handler for NoSuchElementException

--- src/main/java/com/github/zackhotte/pasithea/controller/ProductRestControllerAdvice.java
+++ src/main/java/com/github/zackhotte/pasithea/controller/ProductRestControllerAdvice.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class ProductRestControllerAdvice {
 
@@ -15,6 +19,23 @@ public class ProductRestControllerAdvice {
     protected ResponseEntity<Response> outOfStockExceptionHandler(OutOfStockException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
                 new Response("Out of Stock", e.getMessage(), 403)
+        );
+    }
+
+    @ExceptionHandler(value = {IOException.class, URISyntaxException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Response> ioExceptionHandler(IOException e) {
+        return ResponseEntity.badRequest().body(
+                new Response("Error parsing the request body", e.getMessage(), 400)
+        );
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected ResponseEntity<Response> noSuchElementExceptionHandler(NoSuchElementException e) {
+        return new ResponseEntity<>(
+                new Response("Item id not found", e.getMessage(), 404),
+                HttpStatus.NOT_FOUND
         );
     }
 
