a578eaf1d383f64eb47b691e4d4617a4a584bf4d
zackhot <zack.hotte@gmail.com>
2018-08-05 14:35:30 -0400

    Added contextLoads test

--- src/test/java/com/github/zackhotte/pasithea/ApplicationTests.java
+++ src/test/java/com/github/zackhotte/pasithea/ApplicationTests.java
@@ -1,16 +1,24 @@
 package com.github.zackhotte.pasithea;
 
+import com.github.zackhotte.pasithea.controller.ProductRestController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+    @Autowired
+    private ProductRestController productRestController;
+
     @Test
     public void contextLoads() {
+        assertThat(productRestController).isNotNull();
     }
 
 }
